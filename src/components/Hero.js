5e661ef92e9cd61e0f1cea6479a7a23371442d21
Rekkuzan5 <cstapp88@gmail.com>
2021-12-02 23:48:43 -0600

    Added some base layout content and components.

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={styles.heroSection}>
+      <div className={styles.heroTitle}>
+        <h1>BUILD</h1>
+        <h1>SOMETHING</h1>
+        <h1>FUN</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
